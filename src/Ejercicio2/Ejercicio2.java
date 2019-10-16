f57cab8342246104bef80ad990eaa23050a0831c
ManuelM <descom666999@gmail.com>
2019-10-16 19:14:53 +0200

    Corregido fallo

--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -7,7 +7,7 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         try {
             int respuesta = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un numero mayor que "+NUMERO_MAYOR+" "));
-            if (respuesta<NUMERO_MAYOR) throw new NumberFormatException();
+            if (respuesta<NUMERO_MAYOR) throw new IllegalArgumentException();
             ContarAdelante desdeCero = new ContarAdelante(respuesta);
             ContarAtras hastaCero = new ContarAtras(respuesta);
             Thread t1 = new Thread(desdeCero);
@@ -16,6 +16,8 @@ public class Ejercicio2 {
             t2.start();
         } catch (NumberFormatException nfe ){
             JOptionPane.showMessageDialog(null,"No has introducido un número válido","",JOptionPane.ERROR_MESSAGE);
+        } catch (IllegalArgumentException iae ){
+            JOptionPane.showMessageDialog(null,"El número introducido debe ser mayor de "+NUMERO_MAYOR,"",JOptionPane.ERROR_MESSAGE);
         } catch (Exception e){
             e.printStackTrace();
         }
